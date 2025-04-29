fd302282127e61727d6794a7b0d1a3bf38ea29be
olivier <odelcroi@gmail.com>
2025-04-29 15:00:12 +0200

    create .env for docker compose

--- start-local-mas.sh
+++ start-local-mas.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 set -e
+
+# Source the .env file to load environment variables
+if [ -f .env ]; then
+  source .env
+else
+  echo "Error: .env file not found. Please create a .env file with the required environment variables."
+  exit 1
+fi
+
 MAS_CONF=$PWD/conf
 cd $MAS_HOME
 
-export TCHAP_IDENTITY_SERVER_URL="http://localhost:8083"
+export TCHAP_IDENTITY_SERVER_URL="http://localhost::${IDENTITY_MOCK_PORT}"
 cargo run -- server -c $MAS_CONF/config.local.dev.yaml
